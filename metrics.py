43aa67aad290903148af68ae2b0b5c7c803f1c45
Zhongxia Yan <zxyan@berkeley.edu>
2018-11-20 14:30:47 -0500

    added more metrics

--- metrics.py
+++ metrics.py
@@ -10,9 +10,21 @@ def auroc(y, y_pred):
     return metrics.roc_auc_score(y, y_pred)
 
 
+def auprc(y, y_pred):
+    return metrics.average_precision_score(y, y_pred)
+
+
 def mse(y, y_pred):
     return metrics.mean_squared_error(y, y_pred)
 
 
 def pearson(y, y_pred):
     return scipy.stats.pearsonr(y, y_pred)[0]
+
+
+def spearman(y, y_pred):
+    return scipy.stats.spearmanr(y, y_pred)[0]
+
+
+def kendalltau(y, y_pred):
+    return scipy.stats.kendalltau(y, y_pred)[0]
